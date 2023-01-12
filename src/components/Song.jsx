96964be4e3e586841aaabdc1be05fd005f9d269a
jakhongiralijonov <jakhongirweb@gmail.com>
2023-01-12 21:31:19 +0500

    import fontawesome icons for project

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,12 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPlay , faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons"
 
 const Song= ()=>{
     return (
         <div className="player">
             <h1>Player</h1>
-            <div className="time-control"></div>
-            <p>Start time</p>
+            <div className="time-control">
+                <p>Start time</p>
             <input type='range' />
             <p > End time</p>
+            </div>
+            <div className="play-control">
+        <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size='2x' />
+        <FontAwesomeIcon className="play" size='2x' icon={faPlay}/>
+        <FontAwesomeIcon size='2x' className="skip-forward" icon={faAngleRight}/>
+            </div>
         </div>
     )
 }
